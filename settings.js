568cc4230fb336a3dc576d4f3f342d50003db857
Developer <jairuscylembautista123@gmail.com>
2026-05-02 22:58:25 +0800

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,11 @@
+{
+  "name": "WorldwidePlusSMP-Bot",
+  "server": {
+    "ip": "YOUR_ATERNOS_IP_HERE.aternos.me",
+    "port": 19132
+  },
+  "bot-account": {
+    "username": "ImmortalBot",
+    "type": "offline"
+  }
+    }

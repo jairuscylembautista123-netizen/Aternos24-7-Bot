2590d53cebf1e452b595c7a67a8bd8b50c0e3b53
Developer <jairuscylembautista123@gmail.com>
2026-05-02 22:52:55 +0800

    Update package.js

--- package.js
+++ package.js
@@ -1,1 +1,16 @@
-
+{
+  "name": "worldwide-plus-smp-bot",
+  "version": "1.0.0",
+  "description": "Immortal Bedrock Bot for Aternos",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "bedrock-protocol": "latest",
+    "express": "latest"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  }
+}
